375a275eab54fa01a09b8d893bab0bd231ddcb88
Alberto Ramos <tekcomp@hotmail.com>
2020-01-26 21:14:53 -0500

    This commit includes assertion for toBeA, toInclude, toNotBe, toNotEqual

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,22 +1,35 @@
+const expect = require('expect');
+
 const utils = require('./utils')
+
 it('should add two numbers', () => {
     let a = 33;
     let b = 11;
     let res = utils.add(a, b)
     if (res === (a + b)) {
         // console.log(`should add the ${a} plus ${b} equal to ${res}`)
-    } else if (res !== 44) {
-        throw new Error(`Expeted 44, but got ${res}.`)
-    }
-});
+        expect(res).toBe(44).toBeA('number');
+        // } else if (res !== 44) {
+        //     throw new Error(`Expeted 44, but got ${res}.`)
+        // }
 
+
+    };
+});
 it('should square a number ', () => {
-    const x = 3
+    const x = 3;
 
     let res = utils.square(x)
-    if (res === (x * x)) {
-        // console.log(`should square ${x} to equal ${res}`);
-    } else if (res !== 44) {
-        throw new Error(`Expected ${(x * x)}, but got ${res}.`)
-    }
+    // if (res === (x * x)) {
+    //     // console.log(`should square ${x} to equal ${res}`);
+    // } else if (res !== 40) {
+    //     throw new Error(`Expected ${(x * x)}, but got ${res}.`)
+    // }
+    // expect(res).toBe(9).toBeA('number');
+    expect([2, 3, 4]).toInclude(2);
+});
+
+it('should expect some value', () => {
+    // expect(12).toNotBe(12);
+    expect({ name: 'alberto' }).toNotEqual({ name: 'Alberto' })
 });
